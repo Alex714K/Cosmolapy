7314a2b887e708ae70db229b286da29f962b7b49
ItzYald <ibutov07@mail.ru>
2024-11-20 13:34:34 +0300

    Добавлен фон, который может ездить вправо-влево

--- scenes/main/MainView.cs
+++ scenes/main/MainView.cs
@@ -11,15 +11,16 @@ public partial class MainView : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        honeyLabel = GetTree().Root.GetNode("Main/MainGui").GetNode<Label>("Resources/Honey");
-        woodLabel = GetTree().Root.GetNode("Main/MainGui").GetNode<Label>("Resources/Wood");
+        honeyLabel = GetTree().Root.GetNode("Main/Camera/MainGui").GetNode<Label>("Resources/Honey");
+        woodLabel = GetTree().Root.GetNode("Main/Camera/MainGui").GetNode<Label>("Resources/Wood");
 		viewModel = new MainViewModel(SetHoneyLabel, SetWoodLabel);
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
+		var childrens = GetChildren();
+
 	}
 
 	void SetHoneyLabel(int newValue)
